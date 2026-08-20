c714d4b433a03f1cd14d81ae65fdb621fc5f95d0
vincentthrives <vincentthrives1127@gmail.com>
2026-08-20 22:04:22 +0530

    test deploy

--- backend/src/main/java/com/vincent/msyep/MsyepApplication.java
+++ backend/src/main/java/com/vincent/msyep/MsyepApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MsyepApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsyepApplication.class, args);
+        
     }
 }
